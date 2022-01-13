3c0bb6112e30f63daa3c0e9e865ac9fbd921b219
JayVnl <jorrit.v@outlook.com>
2022-01-13 12:14:38 +0100

    See changes below:  - Changed directory structure for better scalability  - Created custom header component according to design

--- ARt/Screens/HomeView.swift
+++ ARt/Screens/HomeView.swift
@@ -8,21 +8,25 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        NavigationView {
-            List {
-                ArtCarousel(categoryName: "Uitgelichte Kunst", items: [Art](repeating: Art(id: 0, title: "Children Yellow", author: "author", imageName: "children_yellow"), count: 5))
-                    .listRowInsets(EdgeInsets())
-                
-                ArtList(categoryName: "Uitgelichte Kunst", items: [Art](repeating: Art(id: 0, title: "Children Yellow", author: "author", imageName: "children_yellow", size: "10x12", type: "Schilderij", year: "2001"), count: 5))
-                    .listRowInsets(EdgeInsets())
-            }
-        }
-    }
+	var body: some View {
+		ZStack {
+			ScrollView {
+				VStack(spacing: 0) {
+					Header()
+					
+					ArtCarousel(categoryName: "Uitgelichte Kunst", items: [Art](repeating: Art(id: 0, title: "Children Yellow", author: "author", imageName: "children_yellow"), count: 5))
+						.listRowInsets(EdgeInsets())
+					
+					ArtList(categoryName: "Uitgelichte Kunst", items: [Art](repeating: Art(id: 0, title: "Children Yellow", author: "author", imageName: "children_yellow", size: "10x12", type: "Schilderij", year: "2001"), count: 5))
+						.listRowInsets(EdgeInsets())
+				}
+			}
+		}.ignoresSafeArea(.all, edges: .top)
+	}
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+	static var previews: some View {
+		HomeView()
+	}
 }
